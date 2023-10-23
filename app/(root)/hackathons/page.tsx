51efd1450a0be2f34f9f956bdd48c13ff9502a4e
Ademayowa <ademayowaadeniyi@gmail.com>
2023-10-23 16:13:35 +0100

    chore: UI fixes

--- app/(root)/hackathons/page.tsx
+++ app/(root)/hackathons/page.tsx
@@ -10,13 +10,13 @@ export default async function AllHackathonsPage() {
   return (
     <>
       {/* Hero */}
-      <div className='pb-20 flex bg-[#FEFEFE] items-center lg:flex-row flex-col'>
+      <div className='pb-20 flex bg-[#FAFAFA] !items-center lg:flex-row flex-col'>
         <div className='md:px-20 px-5 pt-10 md:pt-20 lg:w-7/12 w-full xxl:px-96'>
           <h1 className='text-3xl md:text-[56px] font-bold max-w-2xl leading-tight'>
             The Hub for Web3 and Web2{' '}
             <span className='textGradient'>Africa's</span> Hackathons
           </h1>
-          <p className='mt-4 text-lg lg:max-w-md max-w-xl text-[#0e0e11] leading-8'>
+          <p className='mt-4 text-lg lg:max-w-md max-w-xl text-gray-500 leading-8'>
             Deep initiative to foster Web2 and Web3 adoption across Africa.
             Significantly promoting blockchain adoption through bootcamps and
             hackathons.
@@ -24,11 +24,17 @@ export default async function AllHackathonsPage() {
         </div>
 
         <div className='lg:w-6/12 w-full pt-10 md:pt-20 lg:pt-16 md:mr-20 '>
-          <Image src='/pple.jpg' width={800} height={800} alt='hero-image' />
+          <Image
+            src='/pple.jpg'
+            width={800}
+            height={800}
+            alt='hero-image'
+            className='rounded-2xl shadow-lg'
+          />
         </div>
       </div>
 
-      <div className='bg-[#FAFAFA] pb-20 md:px-20 px-5 xxl:px-96 py-10 lg:py-24'>
+      <div className='pb-20 bg-[#FAFAFA] md:px-20 px-5 xxl:px-96 py-10 lg:py-24 mt-0.5'>
         <h2 className='text-3xl font-bold mb-2'>
           Explore <span className='textGradient'>Hackathons</span>
         </h2>
