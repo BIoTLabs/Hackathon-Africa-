fe4bc860ab7599e1c7a554b85b3f1924ac49efa6
Ademayowa <ademayowaadeniyi@gmail.com>
2023-10-23 17:46:37 +0100

    chore: Removed unused schemas

--- sanity/schema.ts
+++ sanity/schema.ts
@@ -1,8 +1,6 @@
 import { type SchemaTypeDefinition } from 'sanity';
 
 import blockContent from './schemas/blockContent';
-import category from './schemas/category';
-import post from './schemas/post';
 import author from './schemas/author';
 import pastHackathon from './schemas/pastHackathon';
 import hackathons from './schemas/hackathons';
@@ -14,9 +12,7 @@ import airdrops from './schemas/airdrops';
 
 export const schema: { types: SchemaTypeDefinition[] } = {
   types: [
-    post,
     author,
-    category,
     blockContent,
     pastHackathon,
     hackathons,
